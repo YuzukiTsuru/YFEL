d789d3700d6b4b36c2b62465d32c99a8ae732e44
YuzukiTsuru <GloomyGhost@GloomyGhost.com>
2022-09-25 15:18:00 +0800

    add reset

--- src/chips/r528.cpp
+++ src/chips/r528.cpp
@@ -5,7 +5,7 @@
 #include "r528.h"
 
 r528::r528(chip_version_t chip_version) : Chips(chip_version) {
-    chip_info.chip_name = "R528-S1/R528-S2/R528-S3/R528-S4/T113-I/T113-S3";
+    chip_info.chip_name = "R528-S1/R528-S2/R528-S3/R528-S4/T113-I/T113-S3/H133";
     chip_info.chip_id = 0x00185900;
     chip_info.chip_type = chip_type_e::Heterogeneous;
     chip_info.chip_core = core_name_.CortexA7;
@@ -27,7 +27,9 @@ chip_function_e r528::chip_detect() {
 }
 
 chip_function_e r528::chip_reset() {
-    return chip_function_e::NotSupport;
+    // Timer::WDOG_IRQ_EN_REG
+    fel_->fel_write32(0x020500a0 + 0x08, (0x16aa << 16) | (0x1 << 0));
+    return chip_function_e::Success;
 }
 
 chip_function_e r528::chip_sid() {
