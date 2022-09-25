5a41e3cd98a53ed6796994b7d08ddf8714f3d75e
YuzukiTsuru <GloomyGhost@GloomyGhost.com>
2022-09-25 15:18:44 +0800

    add chipop to manage chip operate

---
+++ src/chipop.cpp
@@ -0,0 +1,5 @@
+//
+// Created by YuzukiTsuru on 2022/9/25.
+//
+
+#include "chipop.h"
---
+++ src/chipop.h
@@ -0,0 +1,19 @@
+//
+// Created by YuzukiTsuru on 2022/9/25.
+//
+
+#ifndef YFEL_CHIPOP_H
+#define YFEL_CHIPOP_H
+
+#include <QObject>
+#include <QString>
+#include <QtAlgorithms>
+
+class ChipOP : QObject {
+Q_OBJECT
+public:
+    ChipOP();
+};
+
+
+#endif //YFEL_CHIPOP_H
