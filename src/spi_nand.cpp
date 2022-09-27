76fe3ffd838300e11e0cd1da2fc1d11bdf64e2bf
柚木鉉 <GloomyGhost@foxmail.com>
2022-09-27 12:16:01 +0800

    add spi nand interface

---
+++ src/spi_nand.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gloom on 2022/9/27.
+//
+
+#include "spi_nand.h"
---
+++ src/spi_nand.h
@@ -0,0 +1,14 @@
+//
+// Created by gloom on 2022/9/27.
+//
+
+#ifndef YFEL_SPI_NAND_H
+#define YFEL_SPI_NAND_H
+
+
+class spi_nand {
+
+};
+
+
+#endif //YFEL_SPI_NAND_H
